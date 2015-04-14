bf393b2ea8ccbcf6a9e89b9881e5cbcb726a91f2
unknown <pectyo@GH206a-20.marshall.edu>
2015-04-14 15:13:36 -0400

    Project Examples

---
+++ WpfApplication1/WpfApplication1/MainWindow.xaml.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApplication1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        static Timer myTimer;
+        
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+            myTimer = new Timer(3000);
+            myTimer.Elapsed += new ElapsedEventHandler(myTimer_Elapsed);
+            myTimer.Enabled = true;
+            
+
+            
+        }
+        private void changeContent(string content)
+        {
+            //textContent.Text = content;
+
+
+            Action act = () => { textContent.IsReadOnly = !textContent.IsReadOnly; };
+            textContent.Dispatcher.Invoke(act);
+
+        
+        }
+
+        private void myTimer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            //myTimer.Enabled = false;
+            changeContent(DateTime.Now.ToString());
+            
+        
+        }
+
+        private void encrypt(object sender, RoutedEventArgs e)
+        {
+            byte[] keyArray;
+            byte[] toEncryptArray = UTF8Encoding.UTF8.GetBytes(textContent.Text);
+
+            string key = "Password";
+            MD5CryptoServiceProvider hashmd5 = new MD5CryptoServiceProvider();
+            keyArray = hashmd5.ComputeHash(UTF8Encoding.UTF8.GetBytes(key));
+            hashmd5.Clear();
+            TripleDESCryptoServiceProvider tdes = new TripleDESCryptoServiceProvider();
+            
+            tdes.Key = keyArray;
+            tdes.Mode = CipherMode.ECB;
+            tdes.Padding = PaddingMode.PKCS7;
+            ICryptoTransform cTransform = tdes.CreateEncryptor();
+            byte[] resultArray = cTransform.TransformFinalBlock(toEncryptArray, 0,
+      toEncryptArray.Length);
+            tdes.Clear();
+            textContent.Text = Convert.ToBase64String(resultArray, 0, resultArray.Length);
+        }
+
+        private void decrypt(object sender, RoutedEventArgs e)
+        {
+            byte[] keyArray;
+            //get the byte code of the string
+
+            byte[] toEncryptArray = Convert.FromBase64String(textContent.Text);
+
+            
+            //Get your key from config file to open the lock!
+            string key = "Password";
+
+            
+                MD5CryptoServiceProvider hashmd5 = new MD5CryptoServiceProvider();
+                keyArray = hashmd5.ComputeHash(UTF8Encoding.UTF8.GetBytes(key));
+                hashmd5.Clear();
+           
+            TripleDESCryptoServiceProvider tdes = new TripleDESCryptoServiceProvider();
+            tdes.Key = keyArray;
+            
+            tdes.Mode = CipherMode.ECB;
+            tdes.Padding = PaddingMode.PKCS7;
+
+            ICryptoTransform cTransform = tdes.CreateDecryptor();
+            byte[] resultArray = cTransform.TransformFinalBlock(
+                                 toEncryptArray, 0, toEncryptArray.Length);
+            tdes.Clear();
+            textContent.Text = UTF8Encoding.UTF8.GetString(resultArray);
+        }
+    }
+}
